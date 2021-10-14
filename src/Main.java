b6adbeb3b4e912b6cb524a568b441b8bf728000b
arthur <arthur.low.2018@uni.strath.ac.uk>
2021-10-14 16:59:33 +0100

    ajustments to be made to WMC and RFC and lists of all test files added for each type of metric so we can keep track of whats currently passing and whats not

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,26 @@ import java.io.FileInputStream;
 
 public class Main {
 
-    private static final String FILE_PATH = "resources/Bank Example/MerchantBank.java";
-//    private static final String FILE_PATH = "resources/foxes-and-rabbits-graph/Field.java";
+
+    /**
+     * RFC/CBO/LCOM - Animal,Counter,Field,FieldStats,Fox,GraphView,GridView,Location,Rabbit,Randomizer,Simulator,SimulatorView
+     * WMC/WMC Complex - GraphView,GridView,Simulator
+     *
+     * RFC/CBO/LCOM - Actor,City,CityGUI,DrawableItem,Item,Location,MissingPassengerException,Passenger,PassengerSource,
+     *                Shuttle,Simulation,Taxi,TaxiCompany,Vehicle
+     *
+     * WMC/WMC Complex - City,CityGUI,Location,Passenger,PassengerSource,Vehicle
+     *
+     * RFC/CBO/LCOM - LogAnalyzer,LogEntry,LogfileCreator,LogfileReader,LoglineTokenizer
+     * WMC/WMC Complex - LogfileReader
+     */
+
+ //   private static final String FILE_PATH = "resources/Bank Example/MerchantBank.java";
+//    private static final String FILE_PATH = "resources/foxes-and-rabbits-graph/SimulatorView.java";
+ //     private static final String FILE_PATH = "resources/taxi-company-later-stage/Vehicle.java";
+    private static final String FILE_PATH = "resources/weblog-analyzer/LoglineTokenizer.java";
+
+
 
     public static void main(String[] args) throws Exception {
 
@@ -18,8 +36,8 @@ public class Main {
         WMC2 wmc2 = new WMC2(cu);
         System.out.println(wmc2.getCount());
 
-          RFC rfc = new RFC(cu);
-          rfc.printMethodOutputs();
+//          RFC rfc = new RFC(cu);
+//          rfc.printMethodOutputs();
 
 //          CBO cbo = new CBO(cu);
 //          cbo.printMethodOutputs();
